a036d2e9218cadfb39af98a4fd498afd4b8926c4
Stephan Wald <swald@basis.com>
2022-05-26 22:00:01 +0200

    added initial cut for CallDemo

---
+++ com/dwcj/extendeddemos/call/CallDemo.java
@@ -0,0 +1,109 @@
+package com.dwcj.extendeddemos.call;
+
+import org.dwcj.App;
+import org.dwcj.Environment;
+import org.dwcj.bbj.BBjVar;
+import org.dwcj.bridge.IDwcjBBjBridge;
+import org.dwcj.controls.Button;
+import org.dwcj.controls.Label;
+import org.dwcj.controls.TextBox;
+import org.dwcj.events.ButtonPushEvent;
+import org.dwcj.exceptions.DwcAppInitializeException;
+import org.dwcj.panels.AppPanel;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class CallDemo extends App {
+
+    private TextBox ipa;
+    private TextBox ipb;
+    private String programsource;
+    private Label result;
+
+    @Override
+    public void run() throws DwcAppInitializeException {
+
+        AppPanel panel = new AppPanel();
+        panel.setStyle("padding","20px");
+        panel.setStyle("display","inline-grid");
+        panel.setStyle("gap","10px");
+
+        Label headline = new Label("<html><h1>Legacy Call And Run Demo</h1>");
+        panel.add(headline);
+
+        Label explanation = new Label("<html><p>This Demo Shows how to invoke the following legacy CALL public program.</p>");
+        panel.add(explanation);
+
+        programsource = new String("0010 ENTER A,B,C,D$\n"+
+                "0020 LET C = A * B\n"+
+                "0030 LET D$=\"The Result is \"+STR(C)+\".\"\n"+
+                "0040 EXIT\n");
+
+        //this could later be changed to a MultiLineEdit so the user of this demo can edit the call
+        Label sourcecode = new Label(programsource);
+        sourcecode.setStyle("font-family","monospace");
+        panel.add(sourcecode);
+
+        ipa = new TextBox("4");
+        ipa.setAttribute("type","number");
+        ipa.setAttribute("label","Value for A");
+        ipa.setStyle("padding-top","40px");
+        panel.add(ipa);
+
+        ipb = new TextBox("5");
+        ipb.setAttribute("type","number");
+        ipb.setAttribute("label","Value for B");
+        ipb.setStyle("padding-top","20px");
+        panel.add(ipb);
+
+        Button exec = new Button("Make CALL");
+        panel.add(exec);
+        exec.onClick(this::onExecClick);
+        
+        result = new Label("");
+        result.setStyle("font-family","monospace");
+        panel.add(result);
+
+    }
+
+    private void onExecClick(ButtonPushEvent buttonPushEvent) {
+
+        File f = null;
+        try {
+            f = File.createTempFile("pgm_",".bbj");
+            f.deleteOnExit();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(f.getAbsolutePath()));
+            writer.write(this.programsource);
+            writer.close();
+        } catch (IOException e) {
+            msgbox(e.getMessage(),0,"Could not create program file");
+            throw new RuntimeException(e);
+        }
+
+        IDwcjBBjBridge h = Environment.getInstance().getDwcjHelper();
+        ArrayList args = new ArrayList<BBjVar>();
+        args.add(new BBjVar(new BigDecimal(ipa.getText())));
+        args.add(new BBjVar(new BigDecimal(ipb.getText())));
+        args.add(new BBjVar(new BigDecimal(0)));
+        args.add(new BBjVar(""));
+
+        BBjVar v = new BBjVar("");
+        BBjVar.BBjGenericType t = v.getType();
+
+        ArrayList<BBjVar> ret = h.call(f.getAbsolutePath(), args);
+
+        String r = "Result: \n";
+        r = r+"A="+ret.get(0).getNumVal()+"\n";
+        r = r+"B="+ret.get(1).getNumVal()+"\n";
+        r = r+"C="+ret.get(2).getNumVal()+"\n";
+        r = r+"D$="+ret.get(3).getStrVal()+"\n";
+        result.setText(r);
+
+
+    }
+}
